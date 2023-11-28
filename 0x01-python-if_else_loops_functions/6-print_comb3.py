fe27e427fb3f031c4e12cc9c13ad32aad7d85048
nyoikem <wnyoike@yahoo.com>
2023-11-28 09:13:23 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(9):
     for j in range(i + 1, 10):
         if i * 10 + j < 89:
             print("{:d}{:d}".format(i, j), end=", ")
-            print("{:d}".format(89))
+print("{:d}".format(89))
