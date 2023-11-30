e8e7ae91473d688da86daa89a03455b0c2123256
nyoikem <wnyoike@yahoo.com>
2023-11-30 07:45:04 -0800

    Task 2, 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(argLen - 1))
     for i in range(1, argLen):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
